fad4c7f394b842c755d8a4dd0433b790dcab30d4
federico.barbero.90@gmail.com <federico.barbero.90@gmail.com@d2f9c676-f95e-1a7b-c81e-b4e12e2c2a7f>
2014-03-03 08:39:33 +0000

    Primer avance con la inscripcion. El usuario puede ingresar su numero de documento y obtener su informacion. Se debe probar con un servidor funcionando.

--- Feria virtual/js/inscription.php
+++ Feria virtual/js/inscription.php
@@ -5,23 +5,41 @@
 
 	$q = intval($_GET['q']);
 
-	$con = mysqli_connect('localhost','root','','test');
+	$con = mysqli_connect('localhost','root','','feriavirtual');
 	if (!$con)
 	  {
 	  die('Could not connect: ' . mysqli_error($con));
 	  }
 
 	mysqli_select_db($con,"ajax_demo");
-	$sql="SELECT * FROM user WHERE id = '".$q."'";
+	$sql="SELECT * FROM jos_comprofiler WHERE dni = '".$q."'";
 
 	$result = mysqli_query($con,$sql);
 
+	$desc = array();
 	while($row = mysqli_fetch_array($result))
 	  {
 	  
-	  echo $row['nombre'];
-	  
+	  $desc["name"] = utf8_encode($row["name"]);
+	  $desc["surname"] = utf8_encode($row["surname"]);
+	  $desc["mail"] = utf8_encode($row["mail"]);
+	  $desc["pass"] = $row["pass"];
+	  $desc["birthday"] = $row["birthday"];
+	  $desc["country"] = utf8_encode($row["country"]);
+	  $desc["state"] = utf8_encode($row["state"]);
+	  $desc["city"] = utf8_encode($row["city"]);
+	  $desc["address"] = utf8_encode($row["address"]);
+	  $desc["phone"] = $row["phone"];
+	  $desc["university"] =  utf8_encode($row["university"]);
+	  $desc["career"] = utf8_encode($row["career"]);
+	  $desc["subjects_approved"] = utf8_encode($row["subjects_approved"]);
+	  $desc["work"] = $row["work"];
+	  /*echo $row['name'] 
+	  	   .$row['surname'];*/
 	  }
 
+	  
+	  echo json_encode($desc);
+	
 	mysqli_close($con);
 ?>
